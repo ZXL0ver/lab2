64cca2ddded0a8ad70deacbd41cd56a8cd751342
ZXL0ver <130565880+ZXL0ver@users.noreply.github.com>
2023-04-12 21:11:21 +0300

    Add files via upload

---
+++ Fabric_metod/Fabric_metod/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fabric_metod
+{
+    public interface ILogger
+{
+    void Log(string message);
+}
+
+public class FileLogger : ILogger
+{
+    private readonly string _logFilePath;
+
+    public FileLogger(string logFilePath)
+    {
+        _logFilePath = logFilePath;
+    }
+
+    public void Log(string message)
+    {
+        using (StreamWriter writer = File.AppendText(_logFilePath))
+        {
+            writer.WriteLine(message);
+        }
+    }
+}
+
+public class RichTextBoxLogger : ILogger
+{
+    private readonly RichTextBox _logTextBox;
+
+    public RichTextBoxLogger(RichTextBox logTextBox)
+    {
+        _logTextBox = logTextBox;
+    }
+
+    public void Log(string message)
+    {
+        _logTextBox.AppendText(message + Environment.NewLine);
+    }
+}
+
+public enum LoggerType
+{
+    File,
+    RichTextBox
+}
+
+public static class LoggerFactory
+{
+    public static ILogger CreateLogger(LoggerType loggerType, string logFilePath = null, RichTextBox logTextBox = null)
+    {
+        switch (loggerType)
+        {
+            case LoggerType.File:
+                return new FileLogger(logFilePath);
+            case LoggerType.RichTextBox:
+                return new RichTextBoxLogger(logTextBox);
+            default:
+                throw new NotSupportedException("Logger type not supported");
+        }
+    }
+}
+}
---
+++ Fabric_metod/Fabric_metod/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fabric_metod
+{
+    public partial class Form1 : Form
+    {
+      //  IShapeFactory creator;        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ILogger logger = LoggerFactory.CreateLogger(LoggerType.RichTextBox, logTextBox: richTextBox1);
+            logger.Log("кнопка 1 нажалась");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ILogger logger = LoggerFactory.CreateLogger(LoggerType.RichTextBox, logTextBox: richTextBox1);
+            logger.Log("форма завантажилась");
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            ILogger logger = LoggerFactory.CreateLogger(LoggerType.RichTextBox, logTextBox: richTextBox1);
+            logger.Log("кнопка 2 нажалась");
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            string logFilePath = @"D:\HomeWork\Projects\Fabric_metod\Fabric_metod\bin\Debug\log.txt";
+            ILogger logger = LoggerFactory.CreateLogger(LoggerType.File, logFilePath: logFilePath);
+            logger.Log("кнопка 3 нажалась");
+        }
+    }
+}
